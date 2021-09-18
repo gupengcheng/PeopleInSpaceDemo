999a7e515513011cca71f839ff63aa3b2365d9f8
安卓攻城狮 <gupengcheng311@gmail.com>
2021-09-18 13:12:24 +0800

    Initialize kotlin_gradle_1_5_20

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+buildscript {
+    val kotlinVersion: String by project
+    println("kotlinVersion = $kotlinVersion")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven(uri("https://plugins.gradle.org/m2/")) // For kotlinter-gradle
+    }
+
+    dependencies {
+        // keeping this here to allow AS to automatically update
+        classpath("com.android.tools.build:gradle:7.0.0")
+
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:${kotlinVersion}")
+
+        with(Deps.Gradle) {
+//            classpath(kotlin)
+//            classpath(kotlinSerialization)
+            classpath(sqlDelight)
+            classpath(shadow)
+            classpath(kotlinter)
+        }
+    }
+}
+
+allprojects {
+    apply(plugin = "org.jmailen.kotlinter")
+
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers")
+        maven(url = "https://jitpack.io")
+    }
+}
