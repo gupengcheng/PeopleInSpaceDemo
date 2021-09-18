3fd6954539d873006756275478e0c5f875b7ea85
安卓攻城狮 <gupengcheng311@gmail.com>
2021-09-18 13:12:24 +0800

    Initialize github_workflow

---
+++ app/build.gradle.kts
@@ -0,0 +1,101 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    compileSdk = Versions.androidCompileSdk
+
+    defaultConfig {
+        applicationId = "com.surrus.peopleinspace"
+        minSdk = Versions.androidMinSdk
+        targetSdk = Versions.androidTargetSdk
+
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.compose
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    packagingOptions {
+        exclude("META-INF/licenses/**")
+        exclude("META-INF/AL2.0")
+        exclude("META-INF/LGPL2.1")
+    }
+}
+
+dependencies {
+    with(Deps.Android) {
+        implementation(material)
+        implementation(osmdroidAndroid)
+    }
+
+    with(Deps.AndroidX) {
+        implementation(lifecycleRuntimeKtx)
+        implementation(lifecycleViewmodelKtx)
+        implementation(activityCompose)
+    }
+
+    with(Deps.Compose) {
+        implementation(ui)
+        implementation(uiGraphics)
+        implementation(uiTooling)
+        implementation(foundationLayout)
+        implementation(material)
+        implementation(navigation)
+        implementation(accompanistCoil)
+        implementation(accompanistPlaceholder)
+    }
+
+    with(Deps.Koin) {
+        implementation(core)
+        implementation(android)
+        implementation(compose)
+        testImplementation(test)
+        testImplementation(testJUnit4)
+    }
+
+    with(Deps.Test) {
+        testImplementation(junit)
+        testImplementation(testCore)
+        testImplementation(robolectric)
+        testImplementation(mockito)
+        androidTestImplementation(testRunner)
+
+        // Compose testing dependencies
+        androidTestImplementation(composeUiTest)
+        androidTestImplementation(composeUiTestJUnit)
+        debugImplementation(composeUiTestManifest)
+    }
+
+    implementation(project(":common"))
+}
